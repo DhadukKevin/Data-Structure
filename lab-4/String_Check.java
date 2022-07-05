55dd7eaf9d81dd770fb3358eca7b218dd4a25dfa
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2022-07-05 17:07:02 +0530

    Add files via upload

---
+++ lab-4/String_Check.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class String_Check{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter String in only ab form: ");
+		String s=sc.nextLine();
+		int flag=0;
+		int count_a=0;
+		int count_b=0;
+
+		for(int i=0;i<s.length();i++){
+			if(s.charAt(i)=='a'){
+				count_a++;
+			}
+			else if(s.charAt(i)=='b'){
+				count_b++;
+			}
+			else{
+				System.out.print("invalid string");
+				flag=1;
+			}
+		}
+		if(flag==0){
+			if(count_a==count_b){
+				System.out.print(s+" String is valid");
+			}
+			else{
+				System.out.print(s+" string not valid");
+			}
+		}
+		
+	}
+}
